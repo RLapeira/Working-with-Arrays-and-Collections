6dc67e3a930bc35cee719d5b8a3e0fa39a3225e3
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 11:40:14 +0200

    Enqueuing and Dequeuing

--- CursoArraysCollections/Program.cs
+++ CursoArraysCollections/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            BusStop busStop = new BusStop();
+            Bus bus = new Bus();
+
+            for (int i = 0; i < 6; i++)
+                busStop.PersonArrive(PassengerGenerator.CreatePassenger());
+
+            busStop.BusArrive(bus);
         }
 
     }
