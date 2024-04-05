ba20d93b529117793a4b58aed0af954d17b54544
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 10:30:24 +0200

    Enumerating Dictionaries

--- CursoArraysCollections/Program.cs
+++ CursoArraysCollections/Program.cs
@@ -6,25 +6,9 @@
         {
             var allRoutes = BusRouteRepository.InitializeRoutes();
 
-            Console.WriteLine("Which route do you want to look up?");
-            int number = int.Parse(Console.ReadLine());
-
-            bool success = allRoutes.TryGetValue(number, out BusRoute answer);
-            // Si existe, devuelve true y el valor por answer
-            // más rápido que:
-
-            // bool success = allRoutes.ContainsKey(number);
-            // Solo devuelve true o false
-
-            // porque solo busca el valor una vez
-
-            if (answer != null)
+            foreach (BusRoute route in allRoutes.Values)
             {
-                Console.WriteLine($"The route you asked for is {answer}"); // {allRoutes[number]}
-            }
-            else
-            {
-                Console.WriteLine($"There is no route with number {number}");
+                Console.WriteLine(route);
             }
         }
     }
