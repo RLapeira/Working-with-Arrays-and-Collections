9748c0b614c695fa305dfa7dcef3a33030493668
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 11:35:40 +0200

    Introducing the Demo: Passengers Queueing

---
+++ CursoArraysCollections/Passenger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArraysCollections
+{
+    public class Passenger
+    {
+        public string Name { get; init; }
+        public string Destination { get; init; }
+        public Passenger(string name, string destination)
+        {
+            this.Name = name;
+            this.Destination = destination;
+        }
+        public override string ToString() => $"{Name} to {Destination}";
+    }
+}
