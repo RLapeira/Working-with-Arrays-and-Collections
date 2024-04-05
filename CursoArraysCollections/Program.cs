f814c96e8788a7be195eac6a11c41a673051b3f3
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 08:16:51 +0200

    Introducing Arrays

--- CursoArraysCollections/Program.cs
+++ CursoArraysCollections/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CursoArraysCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusRoute route40 = new BusRoute(40, "Morecambe", "Preston");
+            BusRoute route42 = new BusRoute(42, "Lancaster", "Blackpool");
+
+            BusRoute[] routes =
+            {
+                route40,
+                route42
+            };
+
+            Console.WriteLine(route40);
+            Console.WriteLine(route42);
+        }
+    }
+}
