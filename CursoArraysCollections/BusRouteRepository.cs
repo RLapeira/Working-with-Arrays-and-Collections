546d8f89884f8e43ddeac0f87334b162c8d1c0ab
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 09:57:06 +0200

    List Initializers

--- CursoArraysCollections/BusRouteRepository.cs
+++ CursoArraysCollections/BusRouteRepository.cs
@@ -4,17 +4,19 @@
     {
         public static List<BusRoute> InitializeRoutes()
         {
-            List<BusRoute> result = new List<BusRoute>();
-            result.Add(new BusRoute(40, new string[] {
-                "Morecambe", "Lancaster", "Garstang", "Preston" }));
-            result.Add(new BusRoute(42, new string[] {
-                "Lancaster", "Garstang", "Blackpool" }));
-            result.Add(new BusRoute(1_000, new string[] {
-                "Test origin", "Test destination" }));
-            result.Add(new BusRoute(5, new string[] {
-                "Overton", "Morecambe", "Carnforth" }));
-            result.Add(new BusRoute(555, new string[] {
-                "Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" }));
+            List<BusRoute> result = new List<BusRoute>
+            {
+                new BusRoute(40, new string[] {
+                    "Morecambe", "Lancaster", "Garstang", "Preston" }),
+                new BusRoute(42, new string[] {
+                    "Lancaster", "Garstang", "Blackpool" }),
+                new BusRoute(1_000, new string[] {
+                    "Test origin", "Test destination" }),
+                new BusRoute(5, new string[] {
+                    "Overton", "Morecambe", "Carnforth" }),
+                new BusRoute(555, new string[] {
+                    "Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" }),
+            };
             return result;
         }
 
