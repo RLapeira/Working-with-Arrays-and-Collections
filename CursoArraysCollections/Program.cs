d3632c1d031a7694ddabad1e1afc1798051894c9
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 10:23:53 +0200

    TryGetValue() and ContainsKey()

--- CursoArraysCollections/Program.cs
+++ CursoArraysCollections/Program.cs
@@ -9,11 +9,18 @@
             Console.WriteLine("Which route do you want to look up?");
             int number = int.Parse(Console.ReadLine());
 
-            BusRoute answer = allRoutes[number];
+            bool success = allRoutes.TryGetValue(number, out BusRoute answer);
+            // Si existe, devuelve true y el valor por answer
+            // más rápido que:
+
+            // bool success = allRoutes.ContainsKey(number);
+            // Solo devuelve true o false
+
+            // porque solo busca el valor una vez
 
             if (answer != null)
             {
-                Console.WriteLine($"The route you asked for is {answer}");
+                Console.WriteLine($"The route you asked for is {answer}"); // {allRoutes[number]}
             }
             else
             {
